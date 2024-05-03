519d7433f9ab487fc65e704e226e4395a5a12d13
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-05-03 23:03:47 +0800

    fix plexautoskip skipnext

--- plugins/plexautoskip/resources/skipper.py
+++ plugins/plexautoskip/resources/skipper.py
@@ -386,11 +386,17 @@ class Skipper():
             self.log.debug("Seek target is the end but no more items in the PlayQueue, using seekTo to prevent loop")
             mediaWrapper.seekTo(mediaWrapper.media.duration - self.CREDIT_SKIP_FIX.get(player.product, 0), player)
         else:
+            self.log.debug("Seek target is the end, stop last media and play next")
             commandDelay = mediaWrapper.commandDelay or self.settings.commandDelay
             time.sleep(commandDelay / 1000)
             player.stop()
             time.sleep(commandDelay / 1000)
-            player.playMedia(pq)
+            # 部分设备SkipNext后，offset为0时，默认是结束，这里调整为10兼容处理
+            player.playMedia(pq, offset=10)
+            # 部分设备SkipNext后，不会自动开始播放，这里兼容处理，5s内每个1s唤醒一次播放
+            for _ in range(5):
+                time.sleep(1)
+                player.play()
         return True
 
     def setVolume(self, mediaWrapper: MediaWrapper, volume: int, lowering: bool) -> None:
