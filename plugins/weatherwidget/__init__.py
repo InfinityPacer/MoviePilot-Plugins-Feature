788d03a385c643aebaa34a95bf34b1777b56a30d
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-05-15 02:04:05 +0800

    优化天气小部件显示效果并支持精准匹配城市

--- plugins/weatherwidget/__init__.py
+++ plugins/weatherwidget/__init__.py
@@ -41,7 +41,7 @@ class WeatherWidget(_PluginBase):
     # 插件图标
     plugin_icon = "https://github.com/InfinityPacer/MoviePilot-Plugins/raw/main/icons/weatherwidget.png"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.2"
     # 插件作者
     plugin_author = "InfinityPacer"
     # 作者主页
